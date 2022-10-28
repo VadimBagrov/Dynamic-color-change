ecfe2719836eae72cbc26450e943b825ff37657e
Vadim Bagrov <110101692+VadimBagrov@users.noreply.github.com>
2022-10-28 21:01:16 +0300

    First version+another version
    In the comments below I gave another variant of writing this code, shorter and more familiar

--- Mini-game/app.js
+++ Mini-game/app.js
@@ -35,3 +35,34 @@ function getRandomColor() {
     return colors[index]
 }
 //Math.random() всегда выдает рандомное значение,random это метод,а Math большой объект с множеством функций для работы с математикой
+
+// -------------------------------------------------------
+// Способы сокращения
+
+// function getRandomColor() {
+//     const index = Math.floor(Math.random() *colors.length)
+//     return colors[index]
+// }
+// можно сократить до
+
+// function getRandomColor() {
+//     return colors[Math.floor(Math.random() *colors.length)]
+// }
+
+// square.addEventListener('mouseover', setColor)
+
+// square.addEventListener('mouseleave', removeColor)
+
+// function setColor(event) {
+//     const element = event.target
+//     const color = getRandomColor() //Сначало получим цвет
+//     element.style.backgroundColor = color // а потом передадим сюда
+//     element.style.boxShadow = `0 0 2px ${color}, 0 0
+//     10px ${color}` //Благодаря этому в css добавляем свойство boxShadow,а с помощью `` можем передавать динамические значение
+// }
+
+// function removeColor(event) {
+//     const element = event.target
+//     element.style.backgroundColor = '#E7E7E7'
+//     element.style.boxShadow = '0 0 2px #E7E7E7'
+// } //эффект убирания цвета на #1d1d1d и не оставляем boxShadow
